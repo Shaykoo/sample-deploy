78a870693d3f5b36f402491f54f7d127966e8e51
abhishek <asapragmatic@gmail.com>
2019-11-17 17:14:47 +0530

    sample deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 4747;
+
+app.get('/',(req,res)=>{
+    res.send('Hello! This is a sample app!')
+})
+
+app.get('/hi',(req,res)=>{
+    res.send(`Hi! ${req.query.name}`)
+})
+app.get('/bye',(req,res)=>{
+    res.send(`Bye! ${req.query.name}`)
+})
+
+app.listen(PORT,()=>{
+    console.log('server is running')
+})
